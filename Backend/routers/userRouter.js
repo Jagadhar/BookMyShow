22d645ca5d1995fd6dc02540c3ea8ce2a7288526
JAGADHAR ROY <165374419+Jagadhar@users.noreply.github.com>
2024-09-05 01:15:03 +0530

    first commit

--- Backend/routers/userRouter.js
+++ Backend/routers/userRouter.js
@@ -5,109 +5,105 @@ import jwt from 'jsonwebtoken';
 
 const router = Router();
 
-
 // Register a new user
-router.post('/register', async (req, res) => {
-    const { name, email, phone, password } = req.body;
-    try {
-        const record = await User.findOne({ email: email });
+router.post('/register', async (req,res) => {
+    let name = req.body.name;
+    let email = req.body.email;
+    let phone = req.body.phone;
+    let password = req.body.password;
+    
 
-        if (record) {
-            return res.json({ success: false, message: "Email is already registered" })
-        }
+    const record = await User.findOne({email:email});
 
-        const newUser = new User({
-            name: name,
-            email: email,
-            phone: phone,
-            password: password
+    if(record){
+        return res.status(400).send({
+            message:"Email is already registered"
+        })
+    }
+    else{
+        const user = new User({
+            name:name,
+            email:email,
+            phone:phone,
+            password:password
         });
+    
+        const result = await user.save();
 
-        const user = await newUser.save();
+        //JWT Token
 
-        const {_id} = await user.toJSON();
-        const token = jwt.sign({_id:_id},process.env.JWT_SECRET);
-
-        res.cookie("jwt", token, {
-            httpOnly: true,
-            maxAge: 24 * 60 * 60 * 1000,
-            sameSite: "None",
-            secure: true, // Ensure this if using HTTPS
+        const {_id} = await result.toJSON();
+        const token = jwt.sign({_id:_id},"secret");
+    
+        res.cookie("jwt", token,{
+            httpOnly:true,
+            maxAge:24*60*60*1000,
         });
-
-        res.json({ success: true, token, message: "Registration && Login successful" });
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "error" });
+    
+        res.send({
+            message:"success",
+        })
     }
 });
 
 
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.json({ success: false, message: "User not Found" })
-        }
-
-        const isPasswordCorrect = (password === user.password);
-        if (!isPasswordCorrect) {
-            return res.json({ success: false, message: "Password is Incorrect" })
-        }
-
-        
-        const token = jwt.sign({_id:user._id},process.env.JWT_SECRET);
-
-        res.cookie("jwt", token, {
-            httpOnly: true,
-            maxAge: 24 * 60 * 60 * 1000,
-            sameSite: "None",
-            secure: true, // Ensure this if using HTTPS
+router.post('/login', async (req,res) =>{
+    const user = await User.findOne({email:req.body.email});
+    if(!user){
+        return res.status(404).send({
+            message:"User not Found",
         });
-
-        res.json({ success: true, token, message: "Login successful" });
-
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: "error" });
     }
+
+    const isPasswordCorrect = (req.body.password === user.password);
+    if(!isPasswordCorrect){
+        return res.status(404).send({
+            message:"Password is Incorrect",
+        });
+    }
+
+    const token = jwt.sign({_id:user._id},"secret");
+
+    res.cookie("jwt", token,{
+        httpOnly:true,
+        maxAge:24*60*60*1000,
+    });
+
+    res.send({
+        message:"success",
+    })
 });
 
-router.get("/user", async (req, res) => {
-    try {
+router.get("/user",async (req,res) =>{
+    try{
         console.log("Cookies: ", req.cookies);
         const cookie = req.cookies['jwt'];
-        // Check if token exists
-        if (!cookie) {
-            return res.status(401).json({ success: false, message: "No token provided" });
+        const claims = jwt.verify(cookie,"secret");
+
+        if(!claims){
+            return res.status(401).send({
+                message:"No token provided",
+            });
         }
 
-        const claims = jwt.verify(cookie, process.env.JWT_SECRET);
+        const user = await User.findOne({_id:claims._id});
 
-        if (!claims) {
-            return res.json({ success: false, message: "Unauthenticated" });
-        }
-
-        const user = await User.findOne({ _id: claims._id });
-
-        if (!user) {
-            return res.json({ success: false, message: "User not found" });
-        }
-
-        const { password, ...data } = await user.toJSON();
+        const {password,...data} = await user.toJSON();
 
         res.send(data)
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ success: false, message: "Server error" });
+    }catch(err){
+        return res.status(401).send({
+            message:"Unauthenticated"
+        });
     }
 });
 
-router.post('/logout', (req, res) => {
-    res.cookie("jwt", "", { maxAge: 0 });
+router.post('/logout',(req,res) =>{
+    res.cookie("jwt","",{maxAge:0});
 
-    res.json({ success: true, message: "Logout successful" });
+    res.send({
+        message:"success",
+    })
 })
 
 
